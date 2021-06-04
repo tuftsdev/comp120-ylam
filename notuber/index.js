651dc010d1d423c0a6ded58c8a1a920bf4bc6ee1
Yeung <scorpio9613@gmail.com>
2021-06-05 01:50:43 +0800

    Add custom marker

--- notuber/index.js
+++ notuber/index.js
@@ -1,25 +1,24 @@
 let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 42.352271, lng: -71.05524200000001 },
-    zoom: 10,
-  });
-}
-
-function loadCar() {
-    x = 42.352271;
-    y = -71.05524200000001;
-    jsonData = '[{"id":"mXfkjrFw", "lat": 42.3453, "long": -71.0464},\
+const jsonData = '[{"id":"mXfkjrFw", "lat": 42.3453, "long": -71.0464},\
     {"id":"nZXB8ZHz", "lat": 42.3662, "long": -71.0621},\
     {"id":"Tkwu74WC", "lat": 42.3603, "long": -71.0547},\
     {"id":"5KWpnAJN", "lat": 42.3472, "long": -71.0802},\
     {"id":"uf5ZrXYw", "lat": 42.3663, "long": -71.0544},\
     {"id":"VMerzMH8", "lat": 42.3542, "long": -71.0704}]';
-    JSON.parse(jsonData).forEach(car => {
-        div = document.createElement("div");
-        div.id = "car";
-        document.getElementById("map").append(div);
+jsonObj = JSON.parse(jsonData);
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 42.352271, lng: -71.05524200000001 },
+    zoom: 14,
+  });
+  const markers = jsonObj.map(car => {
+    return new google.maps.Marker({
+      position: {lat: car['lat'], lng: car['long']},
+    //   label: car['id'],
+      map: map,
+      icon: "./car.png",
     });
-    
+  });
 }
