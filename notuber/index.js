204a9a27cd5d4897966e627b711813b68c215a06
Yeung <scorpio9613@gmail.com>
2021-06-16 02:25:03 +0800

    Work is done

--- notuber/index.js
+++ notuber/index.js
@@ -1,1 +1,33 @@
-let map;const jsonData='[{"id":"mXfkjrFw", "lat": 42.3453, "long": -71.0464},{"id":"nZXB8ZHz", "lat": 42.3662, "long": -71.0621},{"id":"Tkwu74WC", "lat": 42.3603, "long": -71.0547},{"id":"5KWpnAJN", "lat": 42.3472, "long": -71.0802},{"id":"uf5ZrXYw", "lat": 42.3663, "long": -71.0544},{"id":"VMerzMH8", "lat": 42.3542, "long": -71.0704}]';jsonObj=JSON.parse(jsonData);function initMap(){map=new google.maps.Map(document.getElementById("map"),{center:{lat:42.352271,lng:-71.05524200000001},zoom:14,});const markers=jsonObj.map(car=>{return new google.maps.Marker({position:{lat:car.lat,lng:car['long']},map:map,icon:"./car.png",})})}
+var map;
+
+function initMap() {
+    navigator.geolocation.getCurrentPosition(myCoord => {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 42.352271, lng: -71.05524200000001 },
+            zoom: 2,
+        });
+
+        const myLoc = new google.maps.Marker({
+            position: {lat: myCoord.coords.latitude, lng: myCoord.coords.longitude},
+            map: map
+        });
+        
+        request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            console.log(request);
+            if (this.readyState == 4 && this.status == 200) {
+                JSON.parse(this.response).forEach(car => {
+                    return new google.maps.Marker({
+                        position: {lat: car['lat'], lng: car['lng']},
+                        map: map,
+                        icon: "./car.png"});
+                });
+            }
+        }
+        request.open('POST', 'https://jordan-marsh.herokuapp.com/rides', true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        console.log("username=JPgy9YNN&lat="+myCoord.coords.latitude+"&lng="+myCoord.coords.longitude);
+        request.send("username=JPgy9YNN&lat="+myCoord.coords.latitude+"&lng="+myCoord.coords.longitude);    
+    });
+    
+}
