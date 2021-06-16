818c63be958ffd6718d482e4e725de060bebdea5
Yeung <scorpio9613@gmail.com>
2021-06-16 16:47:14 +0800

    Finish Lab 10

--- notuber/README.md
+++ notuber/README.md
@@ -14,3 +14,25 @@ All Work Is My Own
 **Before optimization**: 1.34s loading time and 6.4kB data transferred
 
 **After optimization**: 1.1 loading time and 6.3kB data transferred
+
+---
+
+# Lab 10: The Ride-Hailing Service Part 2
+
+## Work Done
+- Add a marker sitting at user's location using navigator.geolocation
+- Send POST request to ride-hailing API, retrieve information of all the vehicles
+- Dynamically mark all the vehicles on the map
+- Dynamically change infowindow's content that displays the information of the closest car to user upon clicking the marker of user's location
+
+## Collaboration
+All Work Is My Own
+
+## Hours
+4 hour
+
+## Optimization
+
+**Before optimization**: 1.34s loading time and 6.4kB data transferred
+
+**After optimization**: 1.1 loading time and 6.3kB data transferred
--- notuber/index.js
+++ notuber/index.js
@@ -1,22 +1,35 @@
 var map;
+var cars;
+var myLatLng;
+var shortestDistance = Infinity;
+var closestCar;
+
+display = (num, precision) => Number(Math.round(num + "e+" + precision))
 
 function initMap() {
     navigator.geolocation.getCurrentPosition(myCoord => {
+        myLatLng = new google.maps.LatLng({lat: myCoord.coords.latitude, lng: myCoord.coords.longitude});
         map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: 42.352271, lng: -71.05524200000001 },
+            center: myLatLng,
             zoom: 2,
+            minZoom: 1
         });
 
-        const myLoc = new google.maps.Marker({
-            position: {lat: myCoord.coords.latitude, lng: myCoord.coords.longitude},
+        const myMarker = new google.maps.Marker({
+            position: myLatLng,
             map: map
         });
-        
+
         request = new XMLHttpRequest();
         request.onreadystatechange = function() {
-            console.log(request);
             if (this.readyState == 4 && this.status == 200) {
-                JSON.parse(this.response).forEach(car => {
+                cars = JSON.parse(this.response);
+                cars.forEach(car => {
+                    d = google.maps.geometry.spherical.computeDistanceBetween(myLatLng, new google.maps.LatLng({lat: car['lat'], lng: car['lng']}));
+                    if (d < shortestDistance) {
+                        closestCar = car;
+                        shortestDistance = d;
+                    }
                     return new google.maps.Marker({
                         position: {lat: car['lat'], lng: car['lng']},
                         map: map,
@@ -26,8 +39,25 @@ function initMap() {
         }
         request.open('POST', 'https://jordan-marsh.herokuapp.com/rides', true);
         request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        console.log("username=JPgy9YNN&lat="+myCoord.coords.latitude+"&lng="+myCoord.coords.longitude);
-        request.send("username=JPgy9YNN&lat="+myCoord.coords.latitude+"&lng="+myCoord.coords.longitude);    
+        request.send("username=JPgy9YNN&lat="+myCoord.coords.latitude+"&lng="+myCoord.coords.longitude);
+
+        const infowindow = new google.maps.InfoWindow({
+            content: "NA",
+        });
+        myMarker.addListener("click", () => {
+            htmlContent = `<h2>Closest Vehicle</h2><ul><li>username: ${closestCar['username']}<li>ID: ${closestCar['id']}<li>Distance: ${(Math.round(100*shortestDistance/1609.344, 2)/100).toString()} miles</ul>`;
+            infowindow.setContent(htmlContent);
+            infowindow.open(map, myMarker);
+
+            const path = new google.maps.Polyline({
+                path: [myLatLng, {lat: closestCar['lat'], lng: closestCar['lng']}],
+                geodesic: true,
+                strokeColor: "#FF0000",
+                strokeOpacity: 1.0,
+                strokeWeight: 2,
+              });
+              path.setMap(map);
+        });
     });
     
 }
